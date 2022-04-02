fa6ebb2ed509db9c232b7cda65b7cac0404fa10e
JuHyuk Park <wnugr05@naver.com>
2022-04-02 16:45:54 +0900

    first commit

---
+++ JHsite/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for JHsite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JHsite.settings')
+
+application = get_wsgi_application()
